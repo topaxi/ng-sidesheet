c394152f9dc4070834c068ae745b31f78da0109b
Damian Senn <senn@puzzle.ch>
2018-10-30 14:48:04 +0100

    Expose sidesheet config from public_api

--- projects/ng-sidesheet/src/public_api.ts
+++ projects/ng-sidesheet/src/public_api.ts
@@ -7,4 +7,5 @@ export * from './lib/components/close-sidesheet.component';
 export * from './lib/components/sidesheet-outlet.component';
 export * from './lib/animations/sidesheet.animations';
 export * from './lib/directives/sidesheet.directive';
+export * from './lib/sidesheet-config';
 export * from './lib/ng-sidesheet.module';
